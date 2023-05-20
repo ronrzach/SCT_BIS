1a04b5864f0e674a8c75e67d1958084efb92830c
ronrzach <62707490+ronrzach@users.noreply.github.com>
2023-05-20 22:06:44 +0530

    Update and rename interface.py to bisviz_interface.py
    Changed file name, inserted main code into a function bisviz() to use in other python code.

--- bisviz_interface.py
+++ bisviz_interface.py
@@ -33,11 +33,14 @@ time_thresh = float(5)
 curr_folder = os.getcwd()
 
 
-csv_file = spa_to_csv(curr_folder, "sample", "H11231513_SNB241927.spa")
 
-# csv_file = curr_folder+"\\"+"tri_event_sample.csv"
 
-event, x_arr, y_arr, e_start, e_end = bisVal_csv(sDate, sTime, eDate, eTime, bis_thresh, time_thresh, csv_file)
-plot_bis(event, x_arr, y_arr, e_start, e_end, bis_thresh, time_thresh)
+csv_file = curr_folder+"\\"+"tri_event_sample.csv"
 
-print("CSV: ", csv_file)
+def bisviz(sDate,sTime,eDate,eTime,bis_thresh, time_thresh,csv_file):
+    event, x_arr, y_arr, e_start, e_end,time_array = bisVal_csv(sDate, sTime, eDate, eTime, bis_thresh, time_thresh, csv_file)
+    plot_bis(event, x_arr, y_arr, e_start, e_end, bis_thresh, time_thresh,time_array)
+
+    print("CSV: ", csv_file)\
+
+bisviz(sDate,sTime,eDate,eTime,bis_thresh, time_thresh,csv_file)
