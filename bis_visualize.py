8cbca6a83b803483d0a322d4413fe195ab38fd49
ronrzach <62707490+ronrzach@users.noreply.github.com>
2023-05-26 02:12:19 +0530

    Updated bis_visualize.py (26/5/23)

--- bis_visualize.py
+++ bis_visualize.py
@@ -53,7 +53,7 @@ def bisVal_csv(sDate, sTime, eDate, eTime, bis_thresh, time_thresh, filename ="t
 
             #rDT[0] and rDT[1] are used to access date and time respectively
             print(rDT[0], rDT[1])
-            time_array.append(rDT[1][:-3])
+
 
             # Code to set value of flag to 1 if rDT[0] and rDT[1] are equal to start date and start time (i.e., sDate and sTime)
             if rDT[0].strip() == sDate.strip() and rDT[1].strip() == sTime.strip():
@@ -61,15 +61,17 @@ def bisVal_csv(sDate, sTime, eDate, eTime, bis_thresh, time_thresh, filename ="t
                 flag = 1
 
             # Code to set value of flag to 0 if rDT[0] and rDT[1] are equal to end date and end time (i.e., eDate and eTime)
-            if rDT[0].strip() == eDate.strip() and rDT[1].strip() == eTime.strip():
+            elif rDT[0].strip() == eDate.strip() and rDT[1].strip() == eTime.strip():
                 # print(rDT)
                 flag = 0
 
             # If
             if flag == 1:
+
                 x_plot_counter = x_plot_counter + 1
                 x_arr.append(x_plot_counter)
                 y_arr.append(float(row[1]))
+                time_array.append(rDT[1][:-3])
 
                 # print(float(row[1]),"\n")
 
@@ -100,10 +102,13 @@ def bisVal_csv(sDate, sTime, eDate, eTime, bis_thresh, time_thresh, filename ="t
                     counter = 0
 
 
+
             elif flag == 0:
+
                 x_plot_counter = x_plot_counter + 1
                 x_arr.append(x_plot_counter)
                 y_arr.append(float(row[1]))
+                time_array.append(rDT[1][:-3])
 
                 # print(float(row[1]),"\n")
 
@@ -131,6 +136,7 @@ def bisVal_csv(sDate, sTime, eDate, eTime, bis_thresh, time_thresh, filename ="t
                     print("E_START: ", e_start)
                     print("E_END: ", e_end)
                     counter = 0
+
                 break
     csvf.close()
     return event, x_arr, y_arr, e_start, e_end,time_array
@@ -143,11 +149,11 @@ def plot_bis(event, x_arr, y_arr, e_start, e_end, bis_thresh, time_thresh,time_a
     print("No of events: ", event)
     x_arr = np.array(x_arr)
     y_arr = np.array(y_arr)
-    my_xticks = time_array
+    my_xt = time_array
     fig, ax = plt.subplots(1, figsize=(50, 15))
     ax.grid()
     plt.xticks(rotation=75)
-    plt.xticks(x_arr, my_xticks)
+    plt.xticks(x_arr, my_xt)
     plt.plot(x_arr, y_arr)
     print(e_start)
     print(e_end)
