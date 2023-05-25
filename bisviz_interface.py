71b954c88656e9a4e566bad7c8616895a9e6c7b8
ronrzach <62707490+ronrzach@users.noreply.github.com>
2023-05-26 02:12:47 +0530

    Updated bisviz_interface.py (26/5/23)

--- bisviz_interface.py
+++ bisviz_interface.py
@@ -30,17 +30,34 @@ eTime = "15:05:00"
 bis_thresh = float(50)
 time_thresh = float(5)
 
+
+
 curr_folder = os.getcwd()
-
-
 
 
 csv_file = curr_folder+"\\"+"tri_event_sample.csv"
 
-def bisviz(sDate,sTime,eDate,eTime,bis_thresh, time_thresh,csv_file):
-    event, x_arr, y_arr, e_start, e_end,time_array = bisVal_csv(sDate, sTime, eDate, eTime, bis_thresh, time_thresh, csv_file)
+def bisviz(sDate, sTime, eDate, eTime, bis_thresh, time_thresh, file, csv_spa):
+    bis_thresh = float(bis_thresh)
+    time_thresh = float(time_thresh)
+    if(len(sTime) != 8):
+        sTime=sTime+":00"
+    if (len(eTime) != 8):
+        eTime = eTime + ":00"
+    sDate = str(sDate)
+    eDate = str(eDate)
+    sTime = str(sTime)
+    eTime = str(eTime)
+    curr_folder = os.getcwd()
+
+    if (csv_spa == "CSV"):
+        file = file
+    elif (csv_spa =="SPA"):
+        file = spa_to_csv(curr_folder,"output",file)
+    print("E TIME CURR: ", eTime)
+    event, x_arr, y_arr, e_start, e_end,time_array = bisVal_csv(sDate, sTime, eDate, eTime, bis_thresh, time_thresh, file)
     plot_bis(event, x_arr, y_arr, e_start, e_end, bis_thresh, time_thresh,time_array)
 
-    print("CSV: ", csv_file)\
+    print("CSV: ", file)
 
-bisviz(sDate,sTime,eDate,eTime,bis_thresh, time_thresh,csv_file)
+# bisviz(sDate,sTime,eDate,eTime,bis_thresh, time_thresh,csv_file)
